c98a6bb2c3950a05b91d4206f73a50769f3d36ee
Artem <moiseev@physics.msu.ru>
2023-07-31 17:38:26 +0300

    Initial Commit

---
+++ CodeLayout4-BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeLayout4-BMI
+//
+//  Created by Artem on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
